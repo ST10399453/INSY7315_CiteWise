9ce2f08a89fdd5aaa748a83544945d6155d1b211
Eth@nr250102 <124041552+huneth25@users.noreply.github.com>
2025-09-04 09:58:48 +0200

    Created Empty Activities and XML Pages

--- app/src/main/java/com/example/citewise_mobile/LoginActivity.kt
+++ app/src/main/java/com/example/citewise_mobile/LoginActivity.kt
@@ -112,7 +112,7 @@ class LoginActivity : AppCompatActivity() {
 
                 if (task.isSuccessful) {
                     // TODO: navigate to your home screen
-                    startActivity(Intent(this, DashboardActivity::class.java))
+                    startActivity(Intent(this, ConsultantDashboardActivity::class.java))
                     finish()
                 } else {
                     tilPassword.error = task.exception?.localizedMessage ?: "Login failed"
--- app/src/main/java/com/example/citewise_mobile/RequestServiceStep2Activity.kt
+++ app/src/main/java/com/example/citewise_mobile/RequestServiceStep2Activity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class DashboardActivity : AppCompatActivity() {
+class RequestServiceStep2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_request_service_step2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
