fea091f9e70056e2dce8e70f2fe7a3d7329663f2
Akhilesh <st10281011@vcconnect.edu.za>
2025-09-07 23:14:53 +0200

    Designed the navigation bar for the landing page

---
+++ CiteWise_Web/wwwroot/js/Landing.js
@@ -0,0 +1,14 @@
+﻿
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.getElementById("menu");
+    const navLinks = document.getElementById("navLinks");
+    const closeBtn = document.getElementById("closeBtn");
+
+    menu.addEventListener("click", () => {
+        navLinks.classList.add("active");
+    });
+
+    closeBtn.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
