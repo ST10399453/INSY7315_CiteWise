f4faf3ca88431881ca6347eb07f6dc74557b9cfc
Akhilesh <st10281011@vcconnect.edu.za>
2025-09-08 19:14:44 +0200

    Added styling and image to landing page

--- CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
+++ CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b8345c4bb7591f0f7428d7884a781dabb5a51f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc159c8ec60a3a3f49395e4ea3e2e0956b3b547c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
