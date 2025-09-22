e90e6df2644efbface94d4d06dd5e07f45411c31
ErinChisholm <128675208+ErinChisholm@users.noreply.github.com>
2025-09-22 16:32:27 +0200

    Frontend for request service step 1, 2, 3, & 4 screen and partial backend for service request step 1 and 4

--- app/src/main/java/com/example/citewise_mobile/RequestServiceStep4Activity.kt
+++ app/src/main/java/com/example/citewise_mobile/RequestServiceStep4Activity.kt
@@ -1,16 +1,47 @@
 package com.example.citewise_mobile
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+import java.util.Calendar
 
 class RequestServiceStep4Activity : AppCompatActivity() {
+
+    private lateinit var etDeadline: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_request_service_step4)
 
+        etDeadline = findViewById(R.id.etDeadline)
+
+        etDeadline.setOnClickListener {
+            showDatePickerDialog()
+        }
+
+    }
+
+    private fun showDatePickerDialog(){
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "${selectedDay}/${selectedMonth + 1}/${selectedYear}"
+                etDeadline.setText(selectedDate)
+            },
+            year,
+            month,
+            day
+        )
+        datePickerDialog.show()
     }
 }
