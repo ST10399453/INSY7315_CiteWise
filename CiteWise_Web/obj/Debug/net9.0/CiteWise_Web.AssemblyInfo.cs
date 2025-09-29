11b450a90da0c1ab3a19c8ec2dbdc970edd93cdc
Akhilesh <st10281011@vcconnect.edu.za>
2025-09-29 11:12:52 +0200

    fixed forgot password

--- CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
+++ CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deccc1ba04cbbce614bc50d1df8d88d7e8c7789d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b0c6f7cfb43ac004020ddbd9229f5dadc01e8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
