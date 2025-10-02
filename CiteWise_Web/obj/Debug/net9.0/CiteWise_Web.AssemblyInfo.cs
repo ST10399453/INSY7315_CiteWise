6feff85f5553db94251d737f76960ce9829ca789
Akhilesh <st10281011@vcconnect.edu.za>
2025-10-02 23:40:10 +0200

    completed the styling for the service request block

--- CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
+++ CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34fd3048eb54771c4aa6a518b366e31e22b41c13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0108ae8adc5b585c5ba470ba5bfc7305eb0e7d85")]
 [assembly: System.Reflection.AssemblyProductAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
