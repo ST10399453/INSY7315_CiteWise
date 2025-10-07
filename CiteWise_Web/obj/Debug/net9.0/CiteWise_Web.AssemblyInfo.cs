f0515fe452ccc39fc3f69bd9f91b8799c6a8bfaa
Akhilesh <st10281011@vcconnect.edu.za>
2025-10-07 11:20:47 +0200

    did the UI for the onboarding

--- CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
+++ CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11de39fc405d5985f29f739158fb759d8e1ce800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35db0433d3677f28d3e9fb35c9c26edaaaedf8b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
