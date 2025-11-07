7b369caa5af0cc8535210aadce3e09acf5ab04cb
Akhilesh <st10281011@vcconnect.edu.za>
2025-11-07 13:24:52 +0200

    improved UI for landing page

--- CiteWise_Web/Controllers/HomeController.cs
+++ CiteWise_Web/Controllers/HomeController.cs
@@ -28,6 +28,16 @@ namespace CiteWise_Web.Controllers
             return View();
         }
 
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
