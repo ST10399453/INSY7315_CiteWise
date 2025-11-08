ce3a552f1d715e203de561f8979a254426f23931
Akhilesh <st10281011@vcconnect.edu.za>
2025-11-08 15:35:19 +0200

    student dashboard UI

--- CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
+++ CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3c75a480e731826ad90692bf5000f2906f7925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a64b9c6eeb58c4b34e9225b5f8385d0e5dab86")]
 [assembly: System.Reflection.AssemblyProductAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
