ab2e8f01b402352fb14c03f397dedde1693994b0
Eth@nr250102 <ST10399453@vcconnect.edu.za>
2025-11-13 22:54:22 +0200

    Create Dockerfile

--- CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
+++ CiteWise_Web/obj/Debug/net9.0/CiteWise_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7f24cf64e135861a3bcc27f1a76cce4e8296798")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff06f82c05dd1da2b754068d4959f17c53da9be4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CiteWise_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
